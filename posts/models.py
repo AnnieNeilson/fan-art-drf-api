9151f5ae5524c97ec769e962454bd251c71348b5
AnnieNeilson <bnefnee@gmail.com>
2022-10-17 10:52:51 +0000

    Add input validation for the images

---
+++ posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', 
+        default='../ApiProjectAssets/default_post_image_qgc0xv.jpg',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+    def __str__(self):
+        return f'{self.id} {self.title}'

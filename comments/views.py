d42fee21e2e3090cc7af45dbf8ad7e85b3a28c88
AnnieNeilson <bnefnee@gmail.com>
2022-10-17 12:25:17 +0000

    Add comment app, create comment serializer, comment detail view and serializer

---
+++ comments/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics, permissions
+from fan_art_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='alan', password='pass')
+    
+    def test_can_list_posts(self):
+        alan = User.objects.get(username='alan')
+        Post.objects.create(owner=alan, title='a title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

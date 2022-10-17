eeb61a931fc136aff1ceaf2b7e136d4f2774962a
AnnieNeilson <bnefnee@gmail.com>
2022-10-17 12:48:20 +0000

    create Like app, add create function to Like Serializer

---
+++ likes/serializers.py
@@ -0,0 +1,21 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'post', 'created_at'
+        ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {'detail': 'possible duplicate'}
+            )

d9181a083b08ce5d926c3f1d79991c69d67cd146
AnnieNeilson <bnefnee@gmail.com>
2022-10-18 11:52:26 +0000

    Edit settings to connect API to front end

--- fan_art_drf_api/settings.py
+++ fan_art_drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -60,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'fan-art-drf-api.herokuapp.com',
+    os.environ.get('ALLOWED_HOST'),
     'localhost'
 ]
 
@@ -68,11 +69,12 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
-
+    
 CORS_ALLOW_CREDENTIALS = True
 
 # Application definition

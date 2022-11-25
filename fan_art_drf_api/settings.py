95512115da2f1bbc6d166aa32aec3c88318a0d65
AnnieNeilson <bnefnee@gmail.com>
2022-11-25 17:47:37 +0000

    Edit staticfiles settings

--- fan_art_drf_api/settings.py
+++ fan_art_drf_api/settings.py
@@ -192,9 +192,9 @@ USE_TZ = True
 
 # STATICFILES_STORAGE = (
 #    "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
-#)
+# )
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 # Default primary key field type
